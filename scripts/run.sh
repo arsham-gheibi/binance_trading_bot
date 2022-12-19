06b5032d746bab6123d5d259d0a06d340bbad42e
arsham-gheibi <gheibi.arsham@gmail.com>
2022-12-19 17:47:11 +0330

    Update docker commands

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ python manage.py wait_for_db
 python manage.py migrate
 python manage.py set_precision
 celery -A app purge -f -Q celery,stream_queue1,stream_queue2,stream_queue3,stream_queue4,stream_queue5
-celery -A app beat -s /vol/celery/celerybeat-schedule --detach
+celery -A app beat -s /vol/beat-schedule --detach
 celery -A app worker -c 1 -n beat -Q celery -l INFO &
 celery -A app worker -c 1 -n main_worker1 -Q main_queue1 -l INFO &
 celery -A app worker -c 1 -n main_worker2 -Q main_queue2 -l INFO &
